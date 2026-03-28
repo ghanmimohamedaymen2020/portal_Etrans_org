77260f3216ccc72375f436bed10641d49e20e55e
ghanmimohamedaymen2020 <ghanmimohamedaymen@gmail.com>
2026-03-28 11:04:28 +0100

    chore(env): support env-specific ports

--- run.py
+++ run.py
@@ -21,4 +21,5 @@ def make_shell_context():
 
 if __name__ == "__main__":
     debug_mode = env == "development"
-    app.run(debug=debug_mode, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(debug=debug_mode, host="0.0.0.0", port=port)
